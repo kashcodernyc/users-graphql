1e2416659d90e67dbe56d0cb406ee430c9858fb0
kashcodernyc <prakash_subba08@hotmail.com>
2023-03-18 14:06:15 -0400

    feat(graphql): intializing graphql project

---
+++ fakeData.js
@@ -0,0 +1,23 @@
+export const userList = [
+    {
+        id: 1,
+        name: 'John',
+        username: 'John',
+        age: 36,
+        nationality: 'Canada'
+    },
+    {
+        id: 2,
+        name: 'Kevin',
+        username: 'Kevin',
+        age: 22,
+        nationality: 'Brazil'
+    },
+     {
+        id: 1,
+        name: 'Wang',
+        username: 'Wang',
+        age: 20,
+        nationality: 'China'
+    }
+]
---
+++ schema/resolvers.js
@@ -0,0 +1,10 @@
+import { userList } from "../fakeData.js";
+
+export const resolvers = {
+    Query: {
+        users(){
+            return userList;
+        }
+    }
+};
+
